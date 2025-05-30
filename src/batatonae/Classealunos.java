c5e4027453e9d06e6d1d3040a1beda5d43cf40cf
Alvim1234 <lucasalvimetec@gmail.com>
2025-05-30 10:46:53 -0300

    Adicionado mais métodos

--- src/batatonae/Classealunos.java
+++ src/batatonae/Classealunos.java
@@ -4,4 +4,24 @@ public class Classealunos {
 	String nomeAt; // Abaixo da classs é atributo e não variável
 	float notaAt;
 	String cursoAt;
+	
+	//Método
+	public void MostrarNomeMet() {
+		System.out.println(this.nomeAt);
+	}
+	public void MostrarNotaMet() {
+		System.out.println(this.notaAt);
+	}
+	public void MostrarCursoMet() {
+		System.out.println(this.cursoAt);
+	}
+	public void ReceberNomeMet(String nomePar) {
+		this.nomeAt = nomePar;
+	}
+	public void ReceberNotaMet(float notaPar) {
+		this.notaAt = notaPar;
+	}
+	public void ReceberCursoMet(String cursoPar) {
+		this.cursoAt = cursoPar;
+	}
 }
--- src/batatonae/Classeprincipal.java
+++ src/batatonae/Classeprincipal.java
@@ -8,13 +8,21 @@ public class Classeprincipal {
 		Classealunos cenouraObj = new Classealunos(); // Objeto 2
 		
 		// Funciona, mas está errado
-		batataObj.nomeAt = "Mikael";
-		batataObj.cursoAt = "Informática";
-		batataObj.notaAt = 8.5F;
+		//batataObj.nomeAt = "Mikael";
+		//batataObj.cursoAt = "Informática";
+		//batataObj.notaAt = 8.5F;
 		
-		System.out.println(batataObj.nomeAt); // sysout CTRL + SPACE
-		System.out.println(batataObj.cursoAt);
-		System.out.println(batataObj.notaAt);
+		batataObj.ReceberNomeMet("Mikael");
+		batataObj.ReceberNotaMet(8.5F);
+		batataObj.ReceberCursoMet("Informática");
+		
+		// Funciona, mas está errado
+		//System.out.println(batataObj.nomeAt); // sysout CTRL + SPACE
+		//System.out.println(batataObj.cursoAt);
+		//System.out.println(batataObj.notaAt);
+		batataObj.MostrarNomeMet();
+		batataObj.MostrarNotaMet();
+		batataObj.MostrarCursoMet();
 	}
 
 }
